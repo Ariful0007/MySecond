8dd625140e87bd01fe544d82c6f553a000547761
Ariful0007 <arifulpub143@gmail.com>
2023-04-16 08:51:35 +0600

    Second

---
+++ MySecond/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  MySecond
+//
+//  Created by sang on 25/9/1444 AH.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
